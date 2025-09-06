2d9ab641d66027aea841fd2f38f512ef4cbbfafb
Mohamed Zahran <work.mohamed.zahran.05@gmail.com>
2025-09-06 18:10:24 +0300

    Starting Task 3

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+let user={
+    userName: "Mohamed",
+    userAge: 24,
+    userGender: "Male"
+};
+// console.log(user.userName.length);
+    for (let i =0; i < user.userName.length; i++) {
+    console.log(user.userName[i]);
+}
